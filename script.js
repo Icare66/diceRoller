f94ebcae7fa52b20322b3b355761a731ad6b1ef1
marin <marinvacquier@hotmail.fr>
2022-02-21 16:52:52 +0100

    ajout responsive front end (non factorisé/non animé)

---
+++ script.js
@@ -0,0 +1,28 @@
+// The canvas and his drawing surface
+var canvas = document.getElementById("canvas");
+var drawingSurface = canvas.getContext("2d");
+var player1 = document.getElementById("player1");
+var player2 = document.getElementById("player2");
+var rollButton = document.getElementById
+
+
+player1.addEventListener('click', function(){
+	alert("ok");});
+
+// the game state
+var LOADING0=0;
+var PLAYING=1;
+var OVER=2;
+
+var scoreNeededToWin = 100;
+
+
+var messages = [];
+
+// The dice object
+
+
+// the message object
+
+
+// The game state
